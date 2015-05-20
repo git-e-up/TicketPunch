fca723a0925616c880adda8007d119a463ab0241
Rodrigo Torres <rtdesigns@yahoo.com>
2015-05-20 15:04:39 -0500

    updates to profile and forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  validates :username, uniqueness: true
+  
   has_many :watch_list_entries
   has_many :movies, through: :watch_list_entries
+
 
   def to_s
     self.username
